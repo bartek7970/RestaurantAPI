fc5bcb8923c8a2e0f0bd68f74f34c37bff243dea
bartek7970 <bartek79701@gmail.com>
2023-02-16 09:57:08 +0100

    Dodaj pliki projektów.

---
+++ RestaurantAPI/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace RestaurantAPI
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> Get(int count, int minT, int maxT)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(minT, maxT),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
